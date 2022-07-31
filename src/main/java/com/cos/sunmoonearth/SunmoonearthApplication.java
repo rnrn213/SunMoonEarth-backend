7c8480afcf60e6318864264f224248005a0abd61
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2022-07-31 19:23:49 +0900

    feat: Init commit

---
+++ src/main/java/com/cos/sunmoonearth/SunmoonearthApplication.java
@@ -0,0 +1,13 @@
+package com.cos.sunmoonearth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunmoonearthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunmoonearthApplication.class, args);
+	}
+
+}
